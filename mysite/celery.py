d83011bf55acdbc825809a62e4d9af8cef872f3f
reymond <josephreymond2011@gmail.com>
2020-01-28 12:17:37 +0530

    nanananana

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
-from mysite.core import tasks
-DJANGO_SETTINGS_MODULE = 'mysite.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 app = Celery('mysite')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+from mysite.core import tasks
 app.autodiscover_tasks(tasks.create_random_user_accounts)
